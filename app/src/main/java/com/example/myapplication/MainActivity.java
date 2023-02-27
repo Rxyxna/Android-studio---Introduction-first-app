8c36155cec0274fbd96b8fe5db5772e624699fa5
Rxyxna <Rayanabaga@outlook.com>
2023-02-27 10:09:42 +0100

    Getting started: Del 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "App up and running");
     }
-}
+    @Override
+        protected void onPause() {
+            super.onPause();
+            Log.d("MainActivity", "onPause: Pausing the app");
+        }
+    @Override
+        protected void onResume() {
+            super.onResume();
+            Log.d("MainActivity", "onResume: Now resuming the application");
+        }
+    }
